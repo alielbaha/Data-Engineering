7da690cd363f459e0aa1ef2c10d05da6db7e97e0
MohamedLAABYDY <mohamedlaabydy12@gmail.com>
2026-02-20 22:47:11 +0100

    refactor(facts): join fact_reviews to valid app SCD version at review time

--- dbt/models/marts/facts/fact_reviews.sql
+++ dbt/models/marts/facts/fact_reviews.sql
@@ -20,13 +20,16 @@ with reviews as (
           from {{ this }}
       )
     {% endif %}
-), apps as (
+), apps_scd as (
     select
+        app_version_key,
         app_key,
         app_id,
         developer_key,
-        category_key
-    from {{ ref('dim_apps') }}
+        category_key,
+        dbt_valid_from,
+        dbt_valid_to
+    from {{ ref('dim_apps_scd') }}
 ), dates as (
     select
         date_key,
@@ -37,6 +40,7 @@ with reviews as (
 select
     r.review_key,
     r.review_id,
+    a.app_version_key,
     a.app_key,
     a.developer_key,
     a.category_key,
@@ -45,8 +49,10 @@ select
     r.thumbs_up_count,
     r.review_timestamp
 from reviews r
-join apps a
+join apps_scd a
   on r.app_id = a.app_id
+ and r.review_timestamp >= a.dbt_valid_from
+ and (a.dbt_valid_to is null or r.review_timestamp < a.dbt_valid_to)
 join dates d
   on cast(r.review_timestamp as date) = d.full_date
 where a.app_key is not null
